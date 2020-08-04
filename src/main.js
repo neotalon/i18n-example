fc38249d5f730b52e6b18458602d57b5a31bda32
Leon Arndt <neotalon27@gmail.com>
2020-08-04 10:10:59 +0200

    Add locale and test i18n

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import i18n from "i18n"
+
+i18n.configure({
+  locales:['en', 'de'],
+  directory: __dirname + '/locales'
+});
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+//translate something
+i18n.setLocale('de');
+let greeting = i18n.__('Hello');
+console.log(greeting);
+
+console.log("Hi Leon");
